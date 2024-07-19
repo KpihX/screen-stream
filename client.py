209c62134ac0c58480ff075c61750503bb396ebd
KpihX <kapoivha@gmail.com>
2024-07-19 11:20:03 +0100

    adding of modularity

--- client.py
+++ client.py
@@ -1,67 +1,125 @@
 import os
+import tkinter as tk
 import socket
-import io
-from PIL import Image
-import time
 import pygame
+import io
 
-NB_MAX_IMG = 20
-FPS = 30  # Limite de rafraîchissement à 30 images par seconde
+from dotenv import load_dotenv
+from tkinter import ttk
+from PIL import Image, ImageTk
 
-def receive_screenshot(client_socket):
-    pygame.init()
-    screen = None
-    clock = pygame.time.Clock()
-    
-    # image_count = 0
-    # while image_count < NB_MAX_IMG:  
-    while True:
-        try:
-            size = int.from_bytes(client_socket.recv(4), 'big')
-            print(f"Taille reçue : {size}")
+# To load .env (common env vars)
+load_dotenv()
 
-            img_bytes = bytearray()
-            
-            while len(img_bytes) < size:
-                packet = client_socket.recv(4096)
-                if not packet:
-                    break
-                img_bytes.extend(packet)
-                # print(f"Paquet reçu : {len(packet)} octets")
-            
-            img = Image.open(io.BytesIO(img_bytes)).convert('RGB')
-            print(f"Dimensions de l'image : {img.size}")
-            
-            if screen is None:
-                screen = pygame.display.set_mode(img.size)
-                print(f"Dimensions de la fenêtre Pygame : {img.size}")
-            
-            img_surface = pygame.image.fromstring(img.tobytes(), img.size, img.mode)
-            screen.blit(img_surface, (0, 0))
-            pygame.display.flip()
-            print("Image affichée")
-            
-            # image_count += 1
+# To load the .env.client (client env vars)
+client_path_dotenv = os.path.join(os.path.dirname(__file__), ".env.client")
+load_dotenv(dotenv_path=client_path_dotenv, override=True)
 
-            # Envoyer un signal au serveur pour indiquer que l'image a été reçue et traitée
-            client_socket.sendall(b'1')
-            
-            # Limiter le taux de rafraîchissement
-            clock.tick(FPS)
-            
-            # Gestion des événements Pygame
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
+FPS=int(os.getenv("FPS"))
+CANVAS_HEIGHT=int(os.getenv("CANVAS_HEIGHT"))
+CANVAS_WIDTH=int(os.getenv("CANVAS_WIDTH"))
+SCREEN_ASK_DELAY=int(os.getenv("SCREEN_ASK_DELAY"))
+MAX_SCREEN_BYTE_SIZE=int(os.getenv("MAX_SCREEN_BYTE_SIZE"))
+
+SERVER_ADDRESS=os.getenv("SERVER_ADDRESS")
+SERVER_PORT=int(os.getenv("SERVER_PORT"))
+MAX_CHUNK_SIZE=int(os.getenv("MAX_CHUNK_SIZE"))
+CONTINUATION_SIGNAL=os.getenv("CONTINUATION_SIGNAL")
+
+class ScreenStreamFrame(tk.Frame):
+    def __init__(self, master, server_adress=SERVER_ADDRESS, server_port=SERVER_PORT, canvas_width=CANVAS_WIDTH, canvas_heigth=CANVAS_HEIGHT, screen_ask_delay=SCREEN_ASK_DELAY)-> None:
+        tk.Frame.__init__(self, master)
+        self.master = master
         
-        except Exception as e:
-            print(f"Erreur : {e}")
-            break
+        self.canvas = tk.Canvas(self.master, width=canvas_width, height=canvas_heigth)
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+        
+        self.pause = False
+        self.stop = False
+        
+        self.btn_frame = ttk.Frame(self.master)
+        self.btn_frame.pack(fill=tk.X)
+        
+        self.pause_btn = ttk.Button(self.btn_frame, text="Pause", command=self.toggle_pause)
+        self.pause_btn.pack(side=tk.LEFT, padx=5, pady=5)
+        
+        self.stop_btn = ttk.Button(self.btn_frame, text="Stop", command=self.stop_screen_stream)
+        self.stop_btn.pack(side=tk.LEFT, padx=5, pady=5)
 
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((server_adress, server_port))
+        
+        self.clock = pygame.time.Clock()
+        
+        self.screen_ask_delay = screen_ask_delay
+        self.master.after(self.screen_ask_delay, self.receive_screenshot)
+    def toggle_pause(self)-> None:
+        self.pause = not self.pause
+        self.pause_btn.config(text="Resume" if self.pause else "Pause")
+
+    def stop_screen_stream(self)-> None:
+        self.stop = True
+        if self.photo:
+            del self.photo
+        self.destroy()
+        
+    def receive_screenshot(self, max_screen_byte_size=MAX_SCREEN_BYTE_SIZE, max_chunk_size=MAX_CHUNK_SIZE, continuation_signal=CONTINUATION_SIGNAL, fps=FPS)-> None:
+        if self.stop:
+            self.socket.close()
+            return
+        
+        if not self.pause:
+            try:
+                size = int.from_bytes(self.socket.recv(max_screen_byte_size), 'big')
+                print(f"Received size: {size}")
+                
+                img_bytes = bytearray()
+                
+                while len(img_bytes) < size:
+                    chunk = self.socket.recv(max_chunk_size)
+                    if not chunk:
+                        break
+                    img_bytes.extend(chunk)
+                    # print(f"Received chunk: {len(chunk)} Bytes.")
+                    
+                img = Image.open(io.BytesIO(img_bytes)).convert("RGB")
+                print(f"Received image size: {img.size}.")
+                    
+                # Redimensionning of the image
+                canvas_width = self.canvas.winfo_width()
+                canvas_height = self.canvas.winfo_height()
+                img = img.resize((canvas_width, canvas_height), Image.Resampling.LANCZOS)
+                    
+                    
+                self.photo = ImageTk.PhotoImage(img)
+                self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo)
+                print("Image displayed!")
+                    
+                # Signal to ask for the next image
+                self.socket.sendall(continuation_signal.encode())
+                    
+                # We limit the refreshing rate
+                self.clock.tick(fps)
+            except Exception as e:
+                raise
+            
+            
+        self.master.after(self.screen_ask_delay, self.receive_screenshot)
+    
+class ScreenStreamApp(tk.Frame):
+    def __init__(self, master:tk.Tk) -> None:
+        tk.Frame.__init__(self, master)
+        self.screen_stream_frame = ScreenStreamFrame(self)
+        self.screen_stream_frame.pack()
+        
 def main():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('127.0.0.1', 5001))
-    receive_screenshot(client_socket)
-
+    root = tk.Tk() 
+    app = ScreenStreamApp(root)
+    app.pack()
+    root.title("Screen Stream")
+    root.mainloop()
+    
 if __name__ == '__main__':
     main()
+    
+    input("\nGlad to have served you! Press 'Enter' to quit.")
--- server.py
+++ server.py
@@ -1,53 +1,79 @@
-import socket
-import threading
-from PIL import ImageGrab
-import io
-
-def send_screenshot(client_socket):
-    try:
-        while True:
-            img = ImageGrab.grab().convert('RGB')
-            byte_arr = io.BytesIO()
-            img.save(byte_arr, format='JPEG')
-            img_bytes = byte_arr.getvalue()
-            size = len(img_bytes)
-            
-            if size > 1024 * 1024:  # Vérification si la taille de l'image dépasse 10 Mo
-                print(f"Taille de l'image trop grande : {size} octets")
-                continue
-                
-            print(f"Taille de l'image envoyée : {size} octets")
-            
-            client_socket.sendall(size.to_bytes(4, 'big'))
-            
-            for i in range(0, size, 4096):
-                client_socket.sendall(img_bytes[i:i+4096])
+#!/usr/bin/env python3
+# -*-coding:UTF-8 -*
     
-            # Attendre le signal du client avant d'envoyer la prochaine image
-            signal = client_socket.recv(1)
-            if signal != b'1':
-                print("Signal incorrect reçu, arrêt de l'envoi!")
-                break
-            
-    except (BrokenPipeError, ConnectionResetError):
-        print("Client déconnecté.")
-        client_socket.close()
-    except Exception as e:
-        raise
+import socket
+import os
+import threading
+import io
+import sys
 
+from dotenv import load_dotenv
+from PIL import ImageGrab
 
+# To load .env (common env vars)
+load_dotenv()
 
+# To load the .env.server (server env vars)
+dotenv_path_server = os.path.join(os.path.dirname(__file__), '.env.server')
+load_dotenv(dotenv_path=dotenv_path_server, override=True)
+
+# Acces to environment variables
+SERVER_ADDRESS = os.getenv('SERVER_ADDRESS')
+SERVER_PORT = int(os.getenv('SERVER_PORT'))
+MAX_CONNECTIONS = int(os.getenv('MAX_CONNECTIONS'))
+MAX_SCREEN_SIZE = int(os.getenv('MAX_SCREEN_SIZE'))
+MAX_SCREEN_BYTE_SIZE = int(os.getenv('MAX_SCREEN_BYTE_SIZE'))
+MAX_CHUNK_SIZE=int(os.getenv("MAX_CHUNK_SIZE"))
+CONTINUATION_SIGNAL=os.getenv("CONTINUATION_SIGNAL")
+
+class ScreenStreamServer:
+    def __init__(self, server_address=SERVER_ADDRESS, server_port=SERVER_PORT, max_connections=MAX_CONNECTIONS)-> None:
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((server_address, server_port))
+        self.socket.listen(max_connections)
+        print("Server is running!")
+        
+        while True:
+            client_socket, addr = self.socket.accept()
+            print(f"Client connected: {addr}")
+            threading.Thread(target=self.send_screenshot, args=(client_socket, addr)).start()
+
+    def send_screenshot(self, client_socket:socket.socket, addr, max_screen_size=MAX_SCREEN_SIZE, max_screen_byte_size=MAX_SCREEN_BYTE_SIZE, max_chunk_size=MAX_CHUNK_SIZE, continuation_signal=CONTINUATION_SIGNAL):
+        try:
+            while True:
+                img = ImageGrab.grab().convert('RGB')
+                byte_arr = io.BytesIO()
+                img.save(byte_arr, format='JPEG')
+                img_bytes = byte_arr.getvalue()
+                size = len(img_bytes)
+                
+                if size > max_screen_size:
+                    print(f"Image size to big: {size} octets")
+                    continue
+                
+                print(f"Sent image size: {size} octets")
+                
+                client_socket.sendall(size.to_bytes(max_screen_byte_size, 'big'))
+                
+                for i in range(0, size, max_chunk_size):
+                    client_socket.sendall(img_bytes[i:i+max_chunk_size])
+
+                # We wait for the client signal to send the next screenshot
+                signal = client_socket.recv(1)
+                
+                if signal != continuation_signal.encode():      
+                    print(f"Stopping of the sending!")
+                    break
+        except (BrokenPipeError, ConnectionResetError):
+            print(f"!Client {addr} disconnected!", file=sys.stderr)
+        except Exception as e:
+            raise
 
 def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('127.0.0.1', 5001))
-    server_socket.listen(5)
-    print("Serveur démarré.")
-    
-    while True:
-        client_socket, addr = server_socket.accept()
-        print(f"Client connecté : {addr}")
-        threading.Thread(target=send_screenshot, args=(client_socket,)).start()
-
-if __name__ == '__main__':
+    screen_stream_server = ScreenStreamServer()    
+        
+if __name__ == "__main__":
     main()
+    
+    input("\nGlad to have served you! Press 'Enter' to quit.")
+    
